e376493898adf75c7078aa58c285997b3393df8a
AnPham <anpham1122000@gmail.com>
2024-08-22 23:03:44 +0700

    update-lint-staged

--- src/app/manage/dropdown-avatar.tsx
+++ src/app/manage/dropdown-avatar.tsx
@@ -5,7 +5,7 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -13,25 +13,25 @@ import Link from 'next/link'
 
 const account = {
   name: 'Nguyễn Văn A',
-  avatar: 'https://i.pravatar.cc/150'
+  avatar: 'https://i.pravatar.cc/150',
 }
 
 export default function DropdownAvatar() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon' className='overflow-hidden rounded-full'>
+        <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
           <Avatar>
             <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
             <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent align="end">
         <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href={'/manage/setting'} className='cursor-pointer'>
+          <Link href={'/manage/setting'} className="cursor-pointer">
             Cài đặt
           </Link>
         </DropdownMenuItem>
