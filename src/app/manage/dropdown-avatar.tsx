dec8013884406486f4f3cb0223fe9ebe4f6a7474
AnPham <anpham1122000@gmail.com>
2024-08-25 21:58:18 +0700

    get-me-profile

--- src/app/manage/dropdown-avatar.tsx
+++ src/app/manage/dropdown-avatar.tsx
@@ -11,18 +11,24 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { handleErrorApi } from '@/lib/utils'
+import { useAccountProfile } from '@/queries/useAccount'
 import { useLogoutMutation } from '@/queries/useAuth'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
-const account = {
-  name: 'Nguyễn Văn A',
-  avatar: 'https://i.pravatar.cc/150',
-}
+// const account = {
+//   name: 'Nguyễn Văn A',
+//   avatar: 'https://i.pravatar.cc/150',
+// }
 
 export default function DropdownAvatar() {
   const logoutMutation = useLogoutMutation()
   const router = useRouter()
+  // const { data } = useAccountProfile((data) => {
+  //   console.log('hello', data)
+  // })
+  const { data } = useAccountProfile()
+  const account = data?.payload.data
 
   const logout = async () => {
     if (logoutMutation.isPending) return
@@ -40,13 +46,13 @@ export default function DropdownAvatar() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
           <Avatar>
-            <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
-            <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+            <AvatarImage src={account?.avatar ?? undefined} alt={account?.name} />
+            <AvatarFallback>{account?.name.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
+        <DropdownMenuLabel>{account?.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <Link href={'/manage/setting'} className="cursor-pointer">
