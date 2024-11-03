60ce8eedd632f5f0f8c3b9fba5ad259aa072d263
AnPham <anpham1122000@gmail.com>
2024-11-04 00:04:27 +0700

    fix-avatar-does-not-change-when-edit-another-account

--- src/app/manage/accounts/edit-employee.tsx
+++ src/app/manage/accounts/edit-employee.tsx
@@ -100,6 +100,7 @@ export default function EditEmployee({
       toast({
         description: result.payload.message,
       })
+      reset()
       onSubmitSuccess && onSubmitSuccess()
     } catch (error) {
       handleErrorApi({
@@ -109,12 +110,17 @@ export default function EditEmployee({
     }
   }
 
+  const reset = () => {
+    setId(undefined)
+    setFile(null)
+  }
+
   return (
     <Dialog
       open={Boolean(id)}
       onOpenChange={(value) => {
         if (!value) {
-          setId(undefined)
+          reset()
         }
       }}
     >
